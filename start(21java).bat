b9236fac2567cd6a543ff7e50a6a3e1a40cfcbed
alzxmpic <minedonate@bk.ru>
2025-09-15 11:34:40 +0400

    t.me/reallyworldsdev

---
+++ start(21java).bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 65001
+color 4
+title t.me/reallyworldsdev
+echo.
+echo by t.me/reallyworldsdev
+echo by t.me/reallyworldsdev
+echo by t.me/reallyworldsdev
+echo.
+timeout 5
+
+jdk-21.0.6\bin\java.exe -DPaper.IgnoreJavaVersion=true -Xms512M -Xmx4G -jar server.jar
+
+pause
